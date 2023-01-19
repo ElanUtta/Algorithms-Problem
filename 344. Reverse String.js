9ea750d97d30926f679d2211834ddb234b391547
Elan Utta <34615578+ElanUtta@users.noreply.github.com>
2023-01-19 15:36:35 -0300

    Create 344. Reverse String.js

---
+++ 344. Reverse String.js
@@ -0,0 +1,14 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    for (let i = 0; i < Math.floor(s.length / 2); i++) {
+
+        let aux = s[s.length - i - 1]
+        s[s.length - i - 1] = s[i] 
+        s[i] = aux
+
+
+    }
+};
